198b41a47d3887704b2d7b27eb3e9e69254f38a3
ghkdlxld <ohsg97@gmail.com>
2023-03-12 23:55:00 +0900

    max값 로직

--- 5656_벽돌_깨기/s1.py
+++ 5656_벽돌_깨기/s1.py
@@ -1,4 +1,4 @@
-import sys
+import sys, copy
 sys.stdin = open('sample_input.txt')
 
 T = int(input())
@@ -6,4 +6,13 @@ for tc in range(1, T+1):
     N, W, H = map(int, input().split())
     bricks = [list(map(int, input().split())) for _ in range(H)]
     print(bricks)
-    print('-----------------------')
+
+
+    def brick_break(start, arr):
+        cnt = [0, 0] # idx, 부순 갯수 (max)
+        # 0-W 길이만큼 순회하면서 부순 갯수 세기
+        for w in range(W):
+            pass
+
+    for n in range(N): # 시작점
+        brick_break(0, n, copy.deepcopy(bricks))
