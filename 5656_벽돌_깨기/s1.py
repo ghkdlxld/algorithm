6e81a883ef57482603425d3861bd20d99a7f5739
ghkdlxld <ohsg97@gmail.com>
2023-04-02 23:44:58 +0900

    벽돌깨기 풀이 정리

--- 5656_벽돌_깨기/s1.py
+++ 5656_벽돌_깨기/s1.py
@@ -2,46 +2,18 @@ import sys
 from collections import deque
 sys.stdin = open('sample_input.txt')
 
+
+
 T = int(input())
 for tc in range(1, T+1):
     N, W, H = map(int, input().split())
     bricks = [list(map(int, input().split())) for _ in range(H)]
     print(bricks)
 
+    # 벽돌 떨어뜨리기 N 회 * range(0, W) -> 터뜨린 갯수는 range 단위마다 계산
+    # 떨어뜨릴 w 위치 정하고, 가장 위 벽돌탐지 -> 터뜨릴 벽돌 q?나 갯수 count , -1 표시(중복x)
+    # curl == N 모든 벽돌 떨어뜨리거나, 더이상 터뜨릴 벽돌이 없다면(q가 비었을 경우 or count == 0) continue, ans 최댓값이랑 비교 교체
 
-    di = [0, 0, 1, -1]
-    dj = [1, -1, 0, 0]
+    # 터뜨릴 돌 다 count 했으면 gravity (w 만큼 돌면서 맨 위 벽돌이 0 이 아닐경우 -1 된 곳은 0으로, 맨위 는 아래로 / 만약 맨위가 0이면 -1 다 0으로)
 
-    def bomb(i, j):
-        q = deque([])
-        while q:
-            pass
-
-
-    def gravity():
-        pass
-
-    def dfs(w, curl):
-        if curl == 3:
-            return
-
-        # w 열에서 가장 윗 자리 찾아서 터트리기
-        for h in range(H):
-            if bricks[h][w] != 0:
-                # 터트리기
-                bomb(h, w)
-                break
-            gravity()
-
-    # dfs 내에서 터트리고 함수1 -> 내려주기 함수2
-
-
-    # 0-W 까지 열을 쭉 돌면서 dfs (curl N 회)
-    for j in range(W):
-        dfs(j, 1)
-
-
-    # 부순 벽돌 갯수 많을때마다 갱신
-
-
-    # 기존 갯수 - 부순 벽돌 갯수
+    # 주의 : 다음 경우의수로 들어갈때 이전거 되돌리려면 copy로 넣어서 계산 및 count 재설정 고려!!!!
