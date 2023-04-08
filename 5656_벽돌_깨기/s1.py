96e22610833643d3cd1373ca04fd4d03d34f6fd9
ghkdlxld <ohsg97@gmail.com>
2023-04-08 19:30:04 +0900

    SWEA 벽돌 깨기

--- 5656_벽돌_깨기/s1.py
+++ 5656_벽돌_깨기/s1.py
@@ -1,19 +1,86 @@
-import sys
+import sys, copy
 from collections import deque
 sys.stdin = open('sample_input.txt')
+
+def gravity(bricks_map):
+    for w in range(W):
+        for h in range(H-1, -1, -1):
+            if bricks_map[h][w] == 0 and h != 0:
+                x = 1
+                while h-x >= 0:
+                    if bricks_map[h-x][w] != 0:
+                        bricks_map[h][w] = bricks_map[h-x][w]
+                        bricks_map[h-x][w] = 0
+                        break
+                    x += 1
+    # 방법 2, temp = [] 하나 해놓고 숫자 있는거만 담아서 다시 반복문 돌면서 map 재생성하는 방법
+
+
+def bomb(j, bricks_map):
+    cnt = 0
+    q = deque()
+
+    di = [0, 0, 1, -1]
+    dj = [1, -1, 0, 0]
+
+    # 맨 위 벽돌
+    for h in range(0, H):
+        if bricks_map[h][j] != 0:
+            q.append((h, j))
+            break
+
+    while q:
+        bomb_i, bomb_j = q.popleft()
+
+
+        if bricks_map[bomb_i][bomb_j] != 1:
+            for k in range(4):
+                d = 1
+                while d < bricks_map[bomb_i][bomb_j]:
+                    ni, nj = bomb_i + di[k]*d, bomb_j + dj[k]*d
+                    if 0 <= ni < H and 0 <= nj < W and bricks_map[ni][nj] != 0 and (ni, nj) not in q:
+                        q.append((ni, nj))
+                    d += 1
+        bricks_map[bomb_i][bomb_j] = 0
+        cnt += 1
+
+    gravity(bricks_map)
+
+    return cnt
+
+
+
+def dfs(curl, cnt, bricks_map):
+    global bomb_bricks, N
+
+    if curl == N:
+        if cnt > bomb_bricks:
+            bomb_bricks = cnt
+        return
+
+
+    for w in range(W):
+        tmp_bricks = copy.deepcopy(bricks_map)
+        break_cnt = bomb(w, bricks_map)
+        dfs(curl + 1, cnt + break_cnt, bricks_map)
+        bricks_map = tmp_bricks
 
 
 
 T = int(input())
 for tc in range(1, T+1):
     N, W, H = map(int, input().split())
-    bricks = [list(map(int, input().split())) for _ in range(H)]
-    print(bricks)
+    bricks_map = []
+    bricks_num = 0  # 벽돌 갯수
+    bomb_bricks = 0 # 터뜨린 갯수
 
-    # 벽돌 떨어뜨리기 N 회 * range(0, W) -> 터뜨린 갯수는 range 단위마다 계산
-    # 떨어뜨릴 w 위치 정하고, 가장 위 벽돌탐지 -> 터뜨릴 벽돌 q?나 갯수 count , -1 표시(중복x)
-    # curl == N 모든 벽돌 떨어뜨리거나, 더이상 터뜨릴 벽돌이 없다면(q가 비었을 경우 or count == 0) continue, ans 최댓값이랑 비교 교체
+    for k in range(H):
 
-    # 터뜨릴 돌 다 count 했으면 gravity (w 만큼 돌면서 맨 위 벽돌이 0 이 아닐경우 -1 된 곳은 0으로, 맨위 는 아래로 / 만약 맨위가 0이면 -1 다 0으로)
+        brick = list(map(int, input().split()))
+        bricks_map.append(brick)
+        bricks_num += (W - brick.count(0))
 
-    # 주의 : 다음 경우의수로 들어갈때 이전거 되돌리려면 copy로 넣어서 계산 및 count 재설정 고려!!!!
+    if bricks_num != 0:
+        dfs(0, 0, bricks_map)
+
+    print('#{} {}'.format(tc, bricks_num-bomb_bricks))
